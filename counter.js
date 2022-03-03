796313db980dc53883fee473e34c0a512a7c9c2a
YukaUmezawa <90828055+YukaUmezawa@users.noreply.github.com>
2022-03-03 11:11:04 +0900

    220303

--- counter.js
+++ counter.js
@@ -1,16 +1,25 @@
-let counter=0;
+// let counter=0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter',0);
+}
 
 function count(){
+    let counter=localStorage.getItem('counter');
     counter++;
     // alert(counter);
     document.querySelector('h1').innerHTML=counter;
-    if (counter%2===1){
-        alert(`ODD: counter is now ${counter}`);
-    }
+    // if (counter%15===1){
+    //     alert(`ODD: counter is now ${counter}`);
+    // }
+
+    localStorage.setItem('counter',counter);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML=localStorage.getItem('counter');
     document.querySelector('button').onclick=count;
     // document.querySelector('button').addEventListener('click', count);
+
+    // setInterval(count,1000)
 })
 
